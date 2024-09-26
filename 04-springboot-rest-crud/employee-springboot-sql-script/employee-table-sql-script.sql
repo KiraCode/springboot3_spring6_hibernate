a19a95861d88cf8adaed74d46b5ed3f15c80ca36
KiraCode <tejasdj.5873@gmail.com>
2024-09-26 19:31:04 +0530

    adding employee sql script

---
+++ 04-springboot-rest-crud/employee-springboot-sql-script/employee-table-sql-script.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE  IF NOT EXISTS `employee_directory`;
+USE `employee_directory`;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Data for table `employee`
+--
+
+INSERT INTO `employee` VALUES 
+	(1,'Leslie','Andrews','leslie@justcode.com'),
+	(2,'Emma','Baumgarten','emma@justcode.com'),
+	(3,'Avani','Gupta','avani@justcode.com'),
+	(4,'Yuri','Petrov','yuri@justcode.com'),
+	(5,'Juan','Vega','juan@justcode.com');
