6ab2a01f602a0d81bb701b65c3a8da817685823f
KiraCode <tejasdj.5873@gmail.com>
2024-09-26 17:27:40 +0530

    get only one student data by using @PathVariable

--- 04-springboot-rest-crud/03-springboot-rest-crud-student-pathvariable-get-single-Student/src/main/java/com/example/demo/controller/StudentController.java
+++ 04-springboot-rest-crud/03-springboot-rest-crud-student-pathvariable-get-single-Student/src/main/java/com/example/demo/controller/StudentController.java
@@ -4,22 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api")
 public class StudentController {
 
-	@GetMapping("/student-list")
-	public List<Student> getStudents() {
-		List<Student> studentList = new ArrayList<>();
+	private List<Student> studentList;
+
+	@PostConstruct
+	public void loadData() {
+		studentList = new ArrayList<>();
 		studentList.add(new Student("John", "Wick"));
 		studentList.add(new Student("Ronaki", "Sanki"));
 		studentList.add(new Student("Marry", "Smith"));
+	}
 
+	@GetMapping("/student-list")
+	public List<Student> getStudents() {
 		return studentList;
+	}
+	
+	@GetMapping("/student/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		return studentList.get(studentId);
 	}
 }
