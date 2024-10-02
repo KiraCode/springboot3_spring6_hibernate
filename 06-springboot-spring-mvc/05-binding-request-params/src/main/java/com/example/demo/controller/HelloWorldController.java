a63ddd464f75800ee1b42e4326034dfda1725055
KiraCode <tejasdj.5873@gmail.com>
2024-10-03 04:30:52 +0530

    chaning the http methods to postmapping

--- 06-springboot-spring-mvc/05-binding-request-params/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ 06-springboot-spring-mvc/05-binding-request-params/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -2,7 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -13,7 +14,7 @@ public class HelloWorldController {
 		return "helloworld-form";
 	}
 
-	@RequestMapping("/processFormVersionTwo")
+	@PostMapping("/processFormVersionTwo")
 	public String processFormVersionThree(@RequestParam("studentName") String name, Model model) {
 
 		String result = "Yo! My Friend " + name.toUpperCase();
