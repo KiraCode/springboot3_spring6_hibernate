0d506a5f8f5e120fc215d7ae7610a066acd0ff84
KiraCode <tejasdj.5873@gmail.com>
2024-10-03 06:43:56 +0530

    adding radio button project reading readButton values from props file

---
+++ 06-springboot-spring-mvc/11-form-data-binding-radio-button-read-from-props-file/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.Student;
+
+@Controller
+public class StudentController {
+
+	@Value("${countries}")
+	private List<String> countries;
+
+	@Value("${languages}")
+	private List<String> languages;
+
+	@GetMapping("/showSudentForm")
+	public String showForm(Model model) {
+		Student student = new Student();
+		model.addAttribute("student", student);
+		model.addAttribute("country", countries);
+		model.addAttribute("languages", languages);
+		return "student-form";
+
+	}
+
+	@PostMapping("/processStudentForm")
+	public String processForm(@ModelAttribute("student") Student theStudent) {
+		System.out.println(
+				"First Name : " + theStudent.getFirstName() + " AND " + "Last Name : " + theStudent.getLastName());
+		return "student-confirmation";
+	}
+}
