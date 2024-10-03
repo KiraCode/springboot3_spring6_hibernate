bbe1f1e83a0a52053b428a21e0eed64e6a61e151
KiraCode <tejasdj.5873@gmail.com>
2024-10-03 16:22:08 +0530

    adding new project to find constructor andadding default constructor in entity class

---
+++ 09-springboot-jpa-advanced-mapping/02-jpa-one-to-one-find-instructor/src/main/java/com/example/demo/dao/AppDaoImpl.java
@@ -0,0 +1,32 @@
+package com.example.demo.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Instructor;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+@Repository
+public class AppDaoImpl implements AppDao {
+
+	private EntityManager entityManager;
+
+	@Autowired
+	public AppDaoImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@Override
+	@Transactional
+	public void save(Instructor instructor) {
+		entityManager.persist(instructor);
+	}
+
+	@Override
+	public Instructor findInstructorbyId(int id) {
+		return entityManager.find(Instructor.class, id);
+	}
+
+}
