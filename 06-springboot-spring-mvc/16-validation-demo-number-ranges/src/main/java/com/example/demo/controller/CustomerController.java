c03303fa85761307932bec2140c80e3bd044b704
KiraCode <tejasdj.5873@gmail.com>
2024-10-03 10:09:25 +0530

    adding spring validation for number range

---
+++ 06-springboot-spring-mvc/16-validation-demo-number-ranges/src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.Customer;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class CustomerController {
+
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, trimmerEditor);
+	}
+	
+	@GetMapping("/")
+	public String showForm(Model model) {
+		model.addAttribute("customer", new Customer());
+		return "customer-form";
+	}
+
+	@PostMapping("/processForm")
+	public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+		System.out.println("Last Name [ "+customer.getLastName()+" ]");
+		if (bindingResult.hasErrors()) {
+			return "customer-form";
+		} else {
+			return "customer-confirmation";
+		}
+	}
+}
